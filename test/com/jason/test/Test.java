72c937f6feed994385640637800271ddfa8435a3
XMM00001 <XMM00001@PCXMM00001.corp.root>
2016-10-13 16:36:12 +0800

    asd

--- test/com/jason/test/Test.java
+++
@@ -1,5 +0,0 @@
-package com.jason.test;
-
-public class Test {
-
-}
---
+++ test/com/jason/test/Test2.java
@@ -0,0 +1,10 @@
+package com.jason.test;
+
+import org.junit.Test;
+
+public class Test2 {
+	@Test
+	public void test(){
+		System.out.println("123212");
+	}
+}
